012adf3eeed783ecbfe8d9b13c989a2aef8047d3
Magdalena Golofit <magdalena.g156@gmail.com>
2022-02-15 12:06:44 +0100

    First basic class and function setup

--- qc/multipath.py
+++ qc/multipath.py
@@ -5,4 +5,32 @@
 @mail: lars@stenseng.net
 """
 
+import numpy as np
 from qc.__version__ import __version__
+
+class Multipath:
+    def getmultipath(obs, th, signals, snr, slips):
+    ### Inputs:
+    # obs: observations
+    # th: threshold
+    # signals: the used signal combination
+    # snr: signal to noise ratio
+    # slips: number of slips
+    # 
+    ### Outputs:
+    # sv: satellite value (obj.sv[0])
+    # signals: display the signal combination
+    # thnr: number of times the multipath exceeded the threshold
+    # tstamps: timestamps
+    # mp: multipath
+    #
+    
+    ### Testing
+    sv = 0
+    thnr = 0
+    tstamps = 0
+    mp = 0
+    res = np.array([sv, [signals[0], signals[1]], thnr, [tstamps, mp]])
+    
+    return res
+
